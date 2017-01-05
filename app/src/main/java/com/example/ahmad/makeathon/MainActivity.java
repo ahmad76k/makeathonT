135843e33de060d7de6fca9030d850081c36a370
ahmad <kargariahmad1@gmail.com>
2017-01-05 16:18:40 +0330

    Add Sign In Activity

--- app/src/main/java/com/example/ahmad/makeathon/MainActivity.java
+++ app/src/main/java/com/example/ahmad/makeathon/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.ahmad.makeathon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        final Button signUpBtn=(Button) findViewById(R.id.sign_up1);
+        
+
     }
 }
